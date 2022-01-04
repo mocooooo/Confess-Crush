dd487756cbd4c5766cbf95db30ae16028fc1e1fb
mocooooo <97101866+mocooooo@users.noreply.github.com>
2022-01-04 19:51:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Web cho người thiểu năng",
+    introTitle: 'Ai vậy',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    btnIntro: '???????',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    btnYes: 'Thích vl luôn =)))))',
+    btnNo: 'Đéo',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại thiểu năng như vậy <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    reply: 'Yêu thì yêu mà đéo yêu thì yêu =)))',
+    mess: 'Tớ biết mà 🥰. Yêu bạn một chút 😘😘',
+    messDesc: 'Tối nay 12h, mình call với cậu nha .',
+    btnAccept: 'Có cái lồn nhé <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100014892528847' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
